fd2091b51cd80d05035b0ee97ab5c2ea3769d39d
ethernetboy <antoniomcerqueirajr@gmail.com>
2021-08-04 09:08:39 -0700

    backupsw

---
+++ scripts/diario/diario.py
@@ -0,0 +1,53 @@
+import time
+import paramiko 
+import os
+import datetime
+import glob
+from git import Repo
+
+
+
+now = datetime.datetime.now()
+user = 'root'
+password = 'root'
+enable_password = '\n'
+port=32768
+f0 = open('cisco.txt')
+for ip in f0.readlines():
+       print('Realizando backup do switch:',ip)
+       ip = ip.strip()
+       filename_prefix = '/var/backupsw/' + ip + '/Diario/'
+       ssh = paramiko.SSHClient()
+       ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+       ssh.connect(ip,port, user, password, look_for_keys=False)
+       chan = ssh.invoke_shell()
+       time.sleep(2)
+       chan.send('enable\n')
+       chan.send(enable_password +'\n')
+       time.sleep(1)
+       chan.send('term len 0\n')
+       time.sleep(1)
+       chan.send('sh run\n')
+       time.sleep(20)
+       output = chan.recv(999999)
+
+       filename = "%s_%s_%.2i-%.2i-%i_%.2i-%.2i-%.2i" % (filename_prefix,ip,now.day,now.month,now.year,now.hour,now.minute,now.second)
+       f1 = open(filename, 'a')
+       f1.write(output.decode("utf-8") )
+       f1.close()
+       ssh.close() 
+       f0.close()
+       
+
+       files = glob.glob(filename_prefix + "*")
+       files.sort(key=os.path.getmtime)
+       if len(files) > 5:
+              print(files[0])
+              os.remove(files[0]) 
+
+time.sleep(20)
+repo = Repo('/var/backupsw/')
+repo.git.add(A=True)
+repo.index.commit('teste')
+origin = repo.remote(name='origin')
+origin.push()
